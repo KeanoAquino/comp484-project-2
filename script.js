0151f3faaa762d0433f2f8f246ba0eeea6e4d196
Keano Lorenzo Oliver Roxas Aquino <149033312+KeanoAquino@users.noreply.github.com>
2026-04-16 16:29:16 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,52 +1,87 @@
-$(function() { // Makes sure that your function is called once all the DOM elements of the page are ready to be used.
-    
-    // Called function to update the name, happiness, and weight of our pet in our HTML
-    checkAndUpdatePetInfoInHtml();
-  
-    // When each button is clicked, it will "call" function for that button (functions are below)
-    $('.treat-button').click(clickedTreatButton);
-    $('.play-button').click(clickedPlayButton);
-    $('.exercise-button').click(clickedExerciseButton);
-  
+$(function() { 
+  // Called function to update the name, happiness, and weight of our pet in our HTML
+  checkAndUpdatePetInfoInHtml();
 
+  // Button click events
+  $('.treat-button').click(clickedTreatButton);
+  $('.play-button').click(clickedPlayButton);
+  $('.exercise-button').click(clickedExerciseButton);
+  $('.nap-button').click(clickedNapButton); // New button event listener
+});
+
+// Initialize pet_info object
+var pet_info = {
+  name: "Jiji", 
+  weight: 50, 
+  happiness: 50
+};
+
+// Image Paths
+var imgDefault = "images/default.png";
+var imgHappy = "images/happy.png";
+var imgSad = "images/sad.png";
+var imgSleep = "images/sleep.png"
+
+function clickedTreatButton() {
+  pet_info.happiness += 5; // Increase happiness
+  pet_info.weight += 2;    // Increase weight
+  triggerPetReaction(imgHappy, "Yum! That was delicious!");
+  checkAndUpdatePetInfoInHtml();
+}
+
+function clickedPlayButton() {
+  pet_info.happiness += 10; // Increase happiness
+  pet_info.weight -= 5;     // Decrease weight
+  triggerPetReaction(imgHappy, "So much fun! Let's play again!");
+  checkAndUpdatePetInfoInHtml();
+}
+
+function clickedExerciseButton() {
+  pet_info.happiness -= 10; // Decrease happiness
+  pet_info.weight -= 10;    // Decrease weight
+  triggerPetReaction(imgSad, "Phew... I'm exhausted.");
+  checkAndUpdatePetInfoInHtml();
+}
+
+function clickedNapButton() {
+  // New behavior for the Nap button
+  pet_info.happiness += 5; 
+  pet_info.weight -= 2;    
+  // Swap out imgDefault for imgSleep right here:
+  triggerPetReaction(imgSleep, "Zzz... That was a nice rest.");
+  checkAndUpdatePetInfoInHtml();
+}
+
+// Function to handle the visual notifications and image swapping
+function triggerPetReaction(imagePath, message) {
+  // 1. UNIQUE JQUERY METHOD: .replaceWith()
+  $('.pet-image').replaceWith('<img class="pet-image" src="' + imagePath + '" alt="My Giga Pet">');
+
+  // Clear any existing messages so they don't stack infinitely
+  $('.pet-message').remove();
+
+  // 2. UNIQUE JQUERY METHOD: .after()
+  $('.pet-image').after('<p class="pet-message"><strong>' + pet_info.name + ' says:</strong> ' + message + '</p>');
+}
+
+function checkAndUpdatePetInfoInHtml() {
+  checkWeightAndHappinessBeforeUpdating();  
+  updatePetInfoInHtml();
+}
+
+function checkWeightAndHappinessBeforeUpdating() {
+  // Conditional to prevent key values from going below zero
+  if (pet_info.weight < 0) {
+    pet_info.weight = 0;
+  }
   
-    
-  })
-  
-    // Add a variable "pet_info" equal to a object with the name (string), weight (number), and happiness (number) of your pet
-    var pet_info = {name:"My Pet Name", weight:"??", happiness:"??"};
-  
-    function clickedTreatButton() {
-      // Increase pet happiness
-      // Increase pet weight
-      checkAndUpdatePetInfoInHtml();
-    }
-    
-    function clickedPlayButton() {
-      // Increase pet happiness
-      // Decrease pet weight
-      checkAndUpdatePetInfoInHtml();
-    }
-    
-    function clickedExerciseButton() {
-      // Decrease pet happiness
-      // Decrease pet weight
-      checkAndUpdatePetInfoInHtml();
-    }
-  
-    function checkAndUpdatePetInfoInHtml() {
-      checkWeightAndHappinessBeforeUpdating();  
-      updatePetInfoInHtml();
-    }
-    
-    function checkWeightAndHappinessBeforeUpdating() {
-      // Add conditional so if weight is lower than zero.
-    }
-    
-    // Updates your HTML with the current values in your pet_info object
-    function updatePetInfoInHtml() {
-      $('.name').text(pet_info['name']);
-      $('.weight').text(pet_info['weight']);
-      $('.happiness').text(pet_info['happiness']);
-    }
-  
+  if (pet_info.happiness < 0) {
+    pet_info.happiness = 0;
+  }
+}
+
+function updatePetInfoInHtml() {
+  $('.name').text(pet_info['name']);
+  $('.weight').text(pet_info['weight']);
+  $('.happiness').text(pet_info['happiness']);
+}
